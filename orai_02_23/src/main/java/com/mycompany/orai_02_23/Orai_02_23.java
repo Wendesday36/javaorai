8a09139efef96ab3c7989b8d66f1c16981a3cff8
Wendesday36 <115298982+Wendesday36@users.noreply.github.com>
2023-02-23 14:56:56 +0100

    Add files via upload

---
+++ orai_02_23/src/main/java/com/mycompany/orai_02_23/Orai_02_23.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.orai_02_23;
+
+/**
+ *
+ * @author berta
+ */
+public class Orai_02_23 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
